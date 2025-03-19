d15efafab9e293e136eab6077df8e50a8e43ad8f
Computador faculdade <lucianomonegatto@gmail.com>
2025-03-19 16:46:51 -0300

    javascript

---
+++ Full Stack/Aula 5/exerciciosextras.js
@@ -0,0 +1,11 @@
+let numero = prompt("Digite um número inteiro positivo:");
+if (numero >= 0){
+    for (let i = numero; i >= 0; i--) {
+        console.log(i);
+    }
+    }
+
+let valor = prompt("Digite um número:");
+for(let i=parseInt(valor);i>=0;i--){
+    console.log(i);
+}
