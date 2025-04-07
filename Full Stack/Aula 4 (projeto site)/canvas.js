7e7a47a5e51542cfa87bebdaed1de374a95c8003
Computador casa <lucianomonegatto@gmail.com>
2025-04-07 11:21:53 -0300

    .

--- Full Stack/Aula 4 (projeto site)/canvas.js
+++ Full Stack/Aula 4 (projeto site)/canvas.js
@@ -15,12 +15,13 @@ function desenhar_arco(ctx,a,b,c,d,e,color,f,g,){
     ctx.strokeStyle = g;
     ctx.stroke();
 }
-function desenhar_triangulo(ctx,a,b,c,d,e,f,cor,i){
+function desenhar_triangulo(ctx,a,b,c,d,e,f,cor,color,i){
     ctx.beginPath()
-    ctx.strokeStyle = cor
     ctx.moveTo(a,b);
     ctx.lineTo(c,d);
     ctx.lineTo(e,f);
+    ctx.strokeStyle = cor
+    ctx.fillStyle = color
     ctx.fill(i);
     ctx.stroke();
 }
@@ -28,7 +29,7 @@ canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 
 desenhar_quadrado(ctx,0,230,299,100,"grey") //chão
-desenhar_quadrado(ctx,0,200,40,200,"#65a1dd") //agua cima pra baixo
+desenhar_quadrado(ctx,0,210,40,200,"#65a1dd") //agua cima pra baixo
 desenhar_quadrado(ctx,30,260,100,40,"#65a1dd") //agua esquerda pra direita
 desenhar_quadrado(ctx,40,190,17,42,"#9a5e22") //arvore esquerda
 desenhar_quadrado(ctx,260,220,17,42,"#9a5e22") //arvore direita
@@ -36,12 +37,12 @@ desenhar_quadrado(ctx,120,140,80,90,"#9a5e22") //casa
 desenhar_quadrado(ctx,168,160,25,25,"lightskyblue"); //janela esquerda
 desenhar_quadrado(ctx,128,160,25,25,"lightskyblue"); //janela direita
 desenhar_quadrado(ctx,153,187,17,43,"#503c28") //porta
-desenhar_triangulo(ctx,120,140,158,90,200,140,"orange")
-desenhar_arco(ctx,240,70,35,0,2,"yellow",4,"yellow")
-desenhar_arco(ctx,268,210,20,0,2,"green",4,"green")
-desenhar_arco(ctx,48,180,20,0,2,"green",4,"green")
-desenhar_arco(ctx,10,220,30,0,2,"#65a1dd",4,"#65a1dd")
-desenhar_arco(ctx,120,291,30,0,2,"#65a1dd",4,"#65a1dd")
+desenhar_triangulo(ctx,120,140,158,90,200,140,"#ff7f3a","#ff7f3a")//telhado
+desenhar_arco(ctx,240,70,35,0,2,"yellow",4,"yellow")//sol
+desenhar_arco(ctx,268,210,20,0,2,"green",4,"green")//folhas arvore direitra
+desenhar_arco(ctx,48,180,20,0,2,"green",4,"green")//folhas arvore esquerda
+desenhar_arco(ctx,8,219,30,0,2,"#65a1dd",4,"#65a1dd")//agua
+desenhar_arco(ctx,120,291.2,30,0,2,"#65a1dd",4,"#65a1dd")//agua
 ctx.moveTo(100,20);
 ctx.lineTo(180,100);
 ctx.lineTo(20,100);
