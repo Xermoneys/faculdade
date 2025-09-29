219e9eddd48cb5534c21fe88a414782bbafb74e9
Computador casa <lucianomonegatto@gmail.com>
2025-09-29 12:30:09 -0300

    Corrreção código atividade

--- 2 semestre/Desenvolvimento de algoritmos/Atividade/Ex_12/Ex_12.c
+++ 2 semestre/Desenvolvimento de algoritmos/Atividade/Ex_12/Ex_12.c
@@ -4,18 +4,18 @@ int main(){
 	int numero;
 	int soma_par = 0;
 	int soma_impar = 0;
-	int maior_valor = 0;
+	int maior_valor;
  for(int i = 1; i < 6; i++){
 		printf("Digite o %d numero: ", i);
 		scanf("%d", &numero);
+		maior_valor = numero;
 		if (numero % 2 == 0){
 			soma_par+= numero;
 	}
 		else if (numero % 2 == 1){
 			soma_impar+= numero;
 	}   else if (numero > maior_valor){
-			maior_valor == numero;
-			return numero;
+			maior_valor = numero;
 }
  }
     
