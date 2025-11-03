6ad9f67320754df37901d920273e06191dc0c769
Computador casa <lucianomonegatto@gmail.com>
2025-11-03 11:22:57 -0300

    mudança no código

--- 2 semestre/Desenvolvimento de algoritmos/Atividade 2/Ex_14/Ex_14.c
+++ 2 semestre/Desenvolvimento de algoritmos/Atividade 2/Ex_14/Ex_14.c
@@ -2,11 +2,13 @@
 //desenvolva um programa que receba via teclado os valores para preencher uma matriz de 3x3 de inteiros. calcule e exiba a soma de todos os elementos do array.
 int main() {
     int matriz[3][3];
+    int soma = 0;
     printf("Digite os valores para a matriz 3x3:\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             printf("Posicao na matriz %d linha %d coluna: ", i+1, j+1);
             scanf("%d", &matriz[i][j]);
+            soma += matriz[i][j];
         }
     }
     printf("\nA matriz sera:\n");
@@ -16,6 +18,7 @@ int main() {
         }
         printf("\n");
     }
+    printf("\nSoma de todos os elementos: %d\n", soma);
     system("pause");
 }
 
