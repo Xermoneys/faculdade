3b0d5118c6b2b818305e83a519726c1854dfcbe5
Xermoneys <lucianomonegatto@gmail.com>
2026-04-13 22:35:24 -0300

    interface de swing

---
+++ 3 semestre/Arquitetura de software/Adiciona/src/main/java/br/edu/adiciona/Adiciona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.adiciona;
+
+/**
+ *
+ * @author feisplmonegatto
+ */
+public class Adiciona {
+
+    public static void main(String[] args) {
+        Jframe_Adiciona telaInicial = new Jframe_Adiciona();
+        telaInicial.setVisible(true);
+    }
+}
