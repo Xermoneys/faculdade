8b7588d15be2bfbd851d0d8672d88c6a5c7f7dcd
LUCIANO VENTURA MONEGATTO <feisplmonegatto@feinet.fei.edu.br>
2026-08-10 22:35:35 -0300

    feat:começando matéria de estrutura de dados.

---
+++ 4 semestre/Estrutura de Dados/array_dinamico.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct array_dinamico
+{
+    int *dados;
+    int tamanho;
+    int capacidade;
+}array_dinamico;
+
+array_dinamico* inicializar_array(int capacidade_inicial){
+
+    array_dinamico *array = malloc(sizeof(array_dinamico));
+    array - >capacidade = capacidade_inicial;
+    array = > 
+
+}
